1916a67f1b7c05324248157b0cfb724e1c0ff377
weifeng <weifeng@zqykj.com>
2017-08-01 21:23:52 +0800

    kafka offset

--- src/main/scala/com/zqykj/bigdata/spark/streaming/SparkStreamingDemo01.scala
+++ src/main/scala/com/zqykj/bigdata/spark/streaming/SparkStreamingDemo01.scala
@@ -3,52 +3,62 @@ package com.zqykj.bigdata.spark.streaming
 import com.zqykj.bigdata.spark.LoggerLevels
 import kafka.serializer.StringDecoder
 import org.apache.log4j.Level
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, TaskContext}
 import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
-import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaManager, KafkaUtils, OffsetRange}
 
 /**
   * Created by fengwei on 17/5/6.
   */
 object SparkStreamingDemo01 {
 
-	def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-		LoggerLevels.setStreamingLogLevels(Level.WARN)
+    LoggerLevels.setStreamingLogLevels(Level.WARN)
 
-		val sparkConf = new SparkConf()
-			.setAppName("early warning")
-			.setMaster("local[4]")
-		val ssc = new StreamingContext(sparkConf, Seconds(3))
+    val sparkConf = new SparkConf()
+      .setAppName("early warning")
+      .setMaster("local[4]")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
 
-		// brokers:kafka的broker 地址， topics: kafka订阅主题
-		val Array(brokers, topics) = Array("Master:9092,Work01:9092,Work03:9092", "detected")
-		val topicsSet = topics.split(",").toSet
+    // brokers:kafka的broker 地址， topics: kafka订阅主题
+    val Array(brokers, topics) = Array("dev62:9092", "test")
+    val topicsSet = topics.split(",").toSet
 
-		// 构造kafka参数
-		val kafkaParams = Map[String, String](
-			"metadata.broker.list" -> brokers,
-			"auto.offset.reset" -> "largest",
-			"group.id" -> "cluster1")
+    // 构造kafka参数
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> brokers,
+      // "auto.offset.reset" -> "largest",
+      "group.id" -> "cluster1")
 
-		// streaming 接收 kafka 的消息
-		val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-			.map(_._2)
-		ssc.checkpoint("checkpoint")
-		// TODO 业务处理
-		val words = messages.flatMap(_.split(" "))
-		println("============>>>" + words)
-		val wordCounts = words.map(x => (x, 1))
-			.reduceByKey((a, b) => a + b)
-		//.reduceByKeyAndWindow(_ + _, _ - _, Minutes(1), Seconds(2), 2)
-		wordCounts.foreachRDD(rdd =>
-			rdd.foreach(println(_))
-		)
-		// 流处理的结果用kafka发送出去
-		//Producer.send("")
-		// 启动spark streaming
-		ssc.start()
-		ssc.awaitTermination()
-	}
+    // kafka直连方式： 指定topic，从指定的offset处开始消费
+    val km = new KafkaManager(kafkaParams)
+    val messages = km.createDirectStream[String, String, StringDecoder, StringDecoder](
+      ssc, kafkaParams, topicsSet)
+
+    // messages.map(record => (record._1, record._2)).print()
+    //    messages.foreachRDD(rdd =>
+    //      rdd.foreachPartition(p => {
+    //        p.foreach(
+    //          f => {
+    //            println(s"offset=${f._1} , value=${f._2}")
+    //          }
+    //        )
+    //      }
+    //      )
+    //    )
+
+    messages.foreachRDD { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd.foreachPartition { iter =>
+        val o: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+        println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
+      }
+
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 
 }
